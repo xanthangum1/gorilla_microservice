367c34add56265361992ff87fd531361f7db31b7
adriankim <adriankim@Adrians-MacBook-Pro.local>
2021-11-25 15:29:59 +0900

    create delete endpoint

---
+++ handlers/delete.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/xanthangum1/gorilla_micro/data"
+)
+
+func (p *Products) DeleteProduct(rw http.ResponseWriter, r *http.Request) {
+	// this will always convert because of the router
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	p.l.Println("handle DELETE Product", id)
+
+	err := data.DeleteProduct(id)
+
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(rw, "Product not found", http.StatusInternalServerError)
+		return
+	}
+}
