368446e417d9e7e7df5aa8d1d441962f883a750d
adriankim18 <adriankim0722@gmail.com>
2021-12-07 15:16:54 +0900

    add bi directional streaming

--- currency/server/currency.go
+++ currency/server/currency.go
@@ -15,17 +15,39 @@ import (
 type Currency struct {
 	rates *data.ExchangeRates
 	log   hclog.Logger
+	// creating a map (dictionary equivalent in Go) with key of subscriberatesserver and value of rates request
+	subscriptions map[protos.Currency_SubscribeRatesServer][]*protos.RateRequest
 }
 
 // NewCurrency creates a new Currency server
 func NewCurrency(r *data.ExchangeRates, l hclog.Logger) *Currency {
-	go func() {
-		ru := r.MonitorRates(5 * time.Second)
-		for range ru {
-			l.Info("Got updated rates")
+	c := &Currency{r, l, make(map[protos.Currency_SubscribeRatesServer][]*protos.RateRequest)}
+	go c.handleUpdates()
+
+	return c
+}
+
+func (c *Currency) handleUpdates() {
+	ru := c.rates.MonitorRates(5 * time.Second)
+	for range ru {
+		c.log.Info("Got Updates Rates")
+		// loop over subscribed clients
+		for k, v := range c.subscriptions {
+
+			// loop over subscribed rates
+			for _, rr := range v {
+				r, err := c.rates.GetRate(rr.GetBase().String(), rr.GetDestination().String())
+				if err != nil {
+					c.log.Error("Unable to get update rate", "base", rr.GetBase().String(), "destination", rr.GetDestination().String())
+				}
+
+				err = k.Send(&protos.RateResponse{Base: rr.Base, Destination: rr.Destination, Rate: r})
+				if err != nil {
+					c.log.Error("Unable to send updated rate", "base", rr.GetBase().String(), "destination", rr.GetDestination())
+				}
+			}
 		}
-	}()
-	return &Currency{r, l}
+	}
 }
 
 // GetRate implements the CurrencyServer GetRate method and returns the currency exchange rate
@@ -37,33 +59,35 @@ func (c *Currency) GetRate(ctx context.Context, rr *protos.RateRequest) (*protos
 	if err != nil {
 		return nil, err
 	}
-	return &protos.RateResponse{Rate: rate}, nil
+	return &protos.RateResponse{Base: rr.Base, Destination: rr.Destination, Rate: rate}, nil
 }
 
 func (c *Currency) SubscribeRates(src protos.Currency_SubscribeRatesServer) error {
 	// make this a go func to prevent two blocks in one function
-	go func() {
-		for {
-			rr, err := src.Recv()
-			if err == io.EOF {
-				c.log.Info("Client has closed connection")
-				break
-			}
-			if err != nil {
-				c.log.Error("Unable to read from clinet", "error", err)
-				break
-			}
-			c.log.Info("Handle client request", "request", rr)
-
-		}
-	}()
-
+	//handles client messages
 	for {
-		err := src.Send(&protos.RateResponse{Rate: 12.1})
+		// Recv is a blocking method which returns on client data
+		rr, err := src.Recv()
+		// io.EOF signals that the client has closed the connection
+		if err == io.EOF {
+			c.log.Info("Client has closed connection")
+			break
+		}
+
+		// any other error means the transport between the server and client is unavailable
 		if err != nil {
+			c.log.Error("Unable to read from clinet", "error", err)
 			return err
 		}
-		time.Sleep(5 * time.Second)
-	}
+		c.log.Info("Handle client request", "request_base", rr.GetBase(), "request_dest", rr.GetDestination())
 
+		rrs, ok := c.subscriptions[src]
+		if !ok {
+			rrs = []*protos.RateRequest{}
+		}
+		rrs = append(rrs, rr)
+		c.subscriptions[src] = rrs
+	}
+	return nil
 }
+{"Base": "GBP", "Destination": "USD"}
